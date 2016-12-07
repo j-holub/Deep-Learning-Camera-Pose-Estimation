2da2b8b8f5af734e7ee62dea5d3b14d49fad05b1
Jan Holub <Jan-Holub@gmx.net>
2016-12-07 16:56:13 +0100

    Basic network structure outlined with comments

---
+++ network.py
@@ -0,0 +1,36 @@
+# TensorFlow
+import tensorflow as tf
+
+
+# ########### #
+# Basic Setup #
+# ########### #
+
+# sensor input
+input_size  = 67
+# output classes (7 degrees of freedom)
+num_classes = 7
+
+# --- Basic Setup --- #
+
+
+
+
+
+# Data Layer
+# 67 neurons
+
+# LSTM Layer
+# 256 neuros
+
+# Fully Connected Layer
+# 128 neurons
+
+# Fully Connected Layer
+# 128  neurons
+
+# Filly Connected Layer
+# 64 neurons
+
+# Pose Estimate
+# 7 neurons
