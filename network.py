eb7dce61cfdcba8e66a3d77d75484abab29ee9c2
Jan Holub <Jan-Holub@gmx.net>
2017-02-14 16:23:34 +0100

    Data is being loaded

--- network.py
+++ network.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 
 # Network
 import networkStructure
+# DataHandler
+import dataHandler
 
 
 # ########### #
@@ -15,8 +17,15 @@ input_size  = 67
 num_classes = 7
 # process one at a time with the LSTM
 time_steps = 1
+# batch size
+batch_size = 15
 
 # --- Basic Setup --- #
+
+
+# set up the data
+data = dataHandler.DataHandler('data/imu_output.txt', 'data/ground_truth.txt', batch_size)
+print(data.next_batch())
 
 # create the network
 network = networkStructure.network(input_size, time_steps)
