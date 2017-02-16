0b704f3ee35bb4a525c9b3ff9e2b401820376a8e
Jan Holub <Jan-Holub@gmx.net>
2017-02-16 15:28:30 +0100

    Added the cost and optimization function

--- network.py
+++ network.py
@@ -20,16 +20,31 @@ time_steps = 1
 # batch size
 batch_size = 15
 
+# training epochs
+epochs = 1
+learning_rate = 0.01
+
 # --- Basic Setup --- #
 
 
 # set up the data
 data = dataHandler.DataHandler('data/imu_output.txt', 'data/ground_truth.txt', batch_size)
-print(data.next_batch())
+
 
 # create the network
 network = networkStructure.network(input_size, time_steps)
 
-
-
+# estimation
 estimate = tf.placeholder(tf.float64, shape=[None, 7])
+
+# cost function and optimization
+cost = tf.reduce_sum(tf.pow(estimate - network, 2)) / (2 * data.training_data_size())
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+
+# initialize session
+init = tf.global_variables_initializer()
+
+# training loop
+with tf.Session() as sess:
+    sess.run(init)
