bfa546964c76465672f815120853cdfa42068ce5
Jan Holub <Jan-Holub@gmx.net>
2017-03-16 16:17:44 +0100

    Initialized the weights for the network randomly devided by the square root of the number of inputs. This was suggested by Karpathy

--- networkStructure.py
+++ networkStructure.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 # Builds the network consisting of
 #   Input layer             67
@@ -28,7 +29,7 @@ def network(input_tensor):
     lstm_cell  = tf.nn.rnn_cell.LSTMCell(256, state_is_tuple=True)
     output, _ = tf.nn.rnn(lstm_cell, dataLayer, dtype=tf.float64)
 
-    lstm_weights = tf.Variable(tf.zeros([256, 128], dtype=tf.float64), dtype=tf.float64)
+    lstm_weights = tf.Variable(tf.random_normal([256, 128], dtype=tf.float64) / np.sqrt(256), dtype=tf.float64)
     lstm_bias    = tf.Variable(tf.zeros([128], dtype=tf.float64), dtype=tf.float64)
 
 
@@ -36,7 +37,7 @@ def network(input_tensor):
     # Fully Connected Layer
     # 128 neurons
     fc1 = tf.tanh(tf.matmul(output[-1], lstm_weights) + lstm_bias)
-    fc1_weights = tf.Variable(tf.zeros([128, 128], dtype=tf.float64), dtype=tf.float64)
+    fc1_weights = tf.Variable(tf.random_normal([128, 128], dtype=tf.float64) / np.sqrt(128), dtype=tf.float64)
     fc1_bias    = tf.Variable(tf.zeros([128], dtype=tf.float64), dtype=tf.float64)
 
 
@@ -46,7 +47,7 @@ def network(input_tensor):
     # 128  neurons
 
     fc2 = tf.tanh(tf.matmul(fc1, fc1_weights) + fc1_bias)
-    fc2_weights = tf.Variable(tf.zeros([128, 64], dtype=tf.float64), dtype=tf.float64)
+    fc2_weights = tf.Variable(tf.random_normal([128, 64], dtype=tf.float64) / np.sqrt(128), dtype=tf.float64)
     fc2_bias    = tf.Variable(tf.zeros([64], dtype=tf.float64), dtype=tf.float64)
 
 
@@ -55,7 +56,7 @@ def network(input_tensor):
     # 64 neurons
 
     fc3 = tf.tanh(tf.matmul(fc2, fc2_weights) + fc2_bias)
-    fc3_weights = tf.Variable(tf.zeros([64, 7], dtype=tf.float64), dtype=tf.float64)
+    fc3_weights = tf.Variable(tf.random_normal([64, 7], dtype=tf.float64) / np.sqrt(64), dtype=tf.float64)
     fc3_bias    = tf.Variable(tf.zeros([7], dtype=tf.float64), dtype=tf.float64)
 
 
